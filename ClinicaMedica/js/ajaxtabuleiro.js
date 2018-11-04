7682d1c576a64eda73a086721899deaf17033870
Zathann <44215217+Zathann@users.noreply.github.com>
2018-11-04 17:35:00 -0200

    Tabuleiro com perguntas

---
+++ ClinicaMedica/js/ajaxtabuleiro.js
@@ -0,0 +1,59 @@
+/**
+ * Função para criar um objeto XMLHTTPRequest
+ */
+function CriaRequest() {
+    try{
+        request = new XMLHttpRequest();        
+    }catch (IEAtual){
+        
+        try{
+            request = new ActiveXObject("Msxml2.XMLHTTP");       
+        }catch(IEAntigo){
+        
+            try{
+                request = new ActiveXObject("Microsoft.XMLHTTP");          
+            }catch(falha){
+                request = false;
+            }
+        }
+    }
+    
+    if (!request) 
+        alert("Seu Navegador não suporta o jogo!");
+    else
+        return request;
+}
+
+/**
+ * Função para enviar os dados
+ */
+function getDados() {
+    
+    // Declaração de Variáveis
+    var result = document.getElementById("resultado");
+    var xmlreq = CriaRequest();
+    
+    // Exibi a imagem de progresso
+    
+    // Monta a requisição
+    xmlreq.open("GET", "pergunta.php", true);
+    
+    // Atribui uma função para ser executada sempre que houver uma mudança de estado
+    xmlreq.onreadystatechange = function(){
+        
+        // Verifica se foi concluído com sucesso e a conexão fechada (readyState=4)
+        if (xmlreq.readyState == 4) {
+            
+            // Verifica se o arquivo foi encontrado com sucesso
+            if (xmlreq.status == 200) {
+                result.innerHTML = xmlreq.responseText;
+            }else{
+                result.innerHTML = "Erro: " + xmlreq.statusText;
+            }
+        }
+    };
+    xmlreq.send(null);
+}
+
+
+
