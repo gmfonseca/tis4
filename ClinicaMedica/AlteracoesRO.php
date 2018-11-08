c82ebdee8e63bf35f1497b1848c177681a84b57b
Rafael Badaró <rafabadaro.15@gmail.com>
2018-11-08 21:09:19 -0200

    Alteracoes de texto e formatação

--- ClinicaMedica/AlteracoesRO.php
+++ ClinicaMedica/AlteracoesRO.php
@@ -119,10 +119,8 @@
                 </div>
                 <div class="row">
                     <br>
-                    <div class="col s12 m6 l6">
 
-                    </div>
-                    <div class="col s12 m6 l6">
+                    <div class="col s12 m12 l12">
                         <h4 class="corVerdeTexto"> Alterações na Face </h4>
                         <p class="topicos"> 
                             O respirador oral apresenta desenvolvimento assimétrico dos músculos, ossos do nariz, lábios e bochechas, provocando efeitos na face: o nariz fica estreito e curto (por falta de uso), bochechas pálidas e baixas, lábio inferior curto, mandíbula posicionada para trás e pouco desenvolvida. Frequentemente, observamos que a criança que respira pela boca apresenta um rosto mais triste e cansado, principalmente pela presença de olheiras ao redor dos olhos.                 
@@ -133,7 +131,7 @@
                 <div class="row">
                     <br>
                     <div class="col s12 m6 l6">
-                        <h4 class="corVerdeTexto"> Alterações Posturais: </h4>
+                        <h4 class="corVerdeTexto"> Alterações Posturais </h4>
                         <p class="topicos"> 
                             Ao respirar pela boca a postura corporal fica seriamente comprometida, pois a criança realiza um movimento de protrusão de cabeça para alargar a dimensão faríngea e entrar mais ar, os ombros são curvados para dentro  tornando  o peito mais afundado; os músculos abdominais ficam flácidos e associado a ingestão de ar causam distensão abdominal;  Para compensar esta alteração da postura da cabeça e da cintura escapular há uma acentuação da lordose lombar,os braços  ficam posicionados para trás e os membros inferiores tendem a ficar mais estendidos e há a tendência do pé ficar mais plano.   
                         </p>
@@ -178,22 +176,19 @@
 
                 <div class="row">
                     <br>
-                    <div class="col s12 m6 l6">
+                    <div class="col s12 m12 l12">
                         <h4 class="corVerdeTexto"> Alterações comportamentais </h4>
                         <p class="topicos"> 
                             Geralmente são crianças inquietas, impacientes, ansiosas, medrosas e impulsivas e apresentam uma irritação constante. À noite quase nunca querem ir para cama, apesar de estarem dormindo em frente da televisão. A respiração exerce forte influência sobre a concentração e os processos de aprendizagem.As crianças que respiram pela boca geralmente apresentam difculdades de atenção e concentração, baixo rendimento escolar e distúrbios na oxigenação do organismo.
                             Acordam com a boca seca. Roncam, porque o ar que entra pela boca faz vibrar os músculos da garganta, gerando o barulho do ronco.
                         </p>
                     </div>
-                    <div class="col s12 m6 l6">
-                    </div>
+                    
                 </div>
 
                 <div class="row">
                     <br>
-                    <div class="col s12 m6 l6">
-                    </div>
-                    <div class="col s12 m6 l6">
+                    <div class="col s12 m12 l12">
                         <h4 class="corVerdeTexto">Alterações das Funções Orais: Mastigação</h4>
                         <p class="topicos"> 
                             A mastigação pode se tornar ineficiente, pois a criança precisa respirar e mastigar ao mesmo tempo, levando-a a mastigar com a boca aberta. 
@@ -211,13 +206,11 @@
 
                 <div class="row">
                     <br>
-                    <div class="col s12 m6 l6">
+                    <div class="col s12 m12 l12">
                         <h4 class="corVerdeTexto"> Alterações das Funções Orais: Fala </h4>
                         <p class="topicos"> 
                             Têm dificuldades para coordenar a respiração com a fala, A criança também poderá apresentar problemas na fala, ou seja, como a língua se encontra flácida e mais para a frente, alguns sons da fala podem se apresentar com distorções, como, por exemplo, ao emitir uma palavra com o som s e z (sala, sapato, zelador, casa, etc.), pode ainda  trocar alguns fonemas como "te", "de" e outros. A língua tende a ficar entre os dentes, o que dificulta a saída do ar e distorce o som. Verificamos com frequência o excesso de saliva durante a fala, ou melhor, algumas pessoas podem falar cuspindo, o que prejudica a comunicação e traz rejeição por parte de quem está próximo ao falante. Ao ouvir um respirador bucal, temos a impressão de que ele está sempre resfriado ou que sua voz está saindo pelo nariz. Esta modificação na voz pode ocorrer porque o nariz pode estar frequentemente entupido. Também não é incomum encontrarmos voz rouca. Isto se justifica pelas crises alérgicas frequentes que acabam irritando todo o trato respiratório e, consequentemente, podem prejudicar a laringe (região localizada na garganta que possui as cordas vocais que produzem o som).
                         </p>
-                    </div>
-                    <div class="col s12 m6 l6">
                     </div>
                 </div>
 
